9289aaf8e3374ee671a5691070e5882fcccbae65
Parto <Parto@Parto-PC>
2014-07-26 16:25:11 +0430

    test

---
+++ mapv2/src/com/example/mapv2/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.mapv2;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+
+	}
+
+	
+
+}
